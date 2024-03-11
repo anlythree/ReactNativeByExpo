e90bdf8ea5692efdb180bdddbef551db15c6d6a2
anlythree <15610462126@163.com>
2024-03-11 11:56:40 +0800

    简单添加一些组件,看一下基本的组件是怎么写的

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      // fragment跟空View不同，它会让它其中的view的属性都显示出来，而如果用View的话，这个View下的属性都将被覆盖
+      <>
+          // justifyContent 是上下对齐。alignItems是左右对齐
+          <View style={{flex : 1 ,justifyContent : 'center', alignItems : 'center'}}>
+              <View>
+                  <Text style={{fontSize:68}}>AnlyThree</Text>
+              </View>
+              <View>
+                  <Text style={{backgroundColor: 'pink'}} >A</Text>
+              </View>
+
+              <View>
+                  <Text style={{backgroundColor: 'gray'}} >B</Text>
+              </View>
+
+          </View>
+      </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
